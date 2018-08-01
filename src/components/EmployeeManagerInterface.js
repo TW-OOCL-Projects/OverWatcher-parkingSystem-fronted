611dbb37123fee665a57604c3fbe92d75c021ccd
JEFFERY LU (ITA-ISDC-ISD-OOCLL/ZHA) <LUJE@OOCL.COM>
2018-08-01 11:27:24 +0800

    [Jeffery] PC端员工管理页面路由跳转

--- src/components/EmployeeManagerInterface.js
+++ src/components/EmployeeManagerInterface.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import MenuList from './MenuList';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import Employees from './Employees';
-import { Layout, Menu, Col,Avatar } from 'antd';
+import { Layout, Menu, Col,Avatar,Icon } from 'antd';
 const { Header, Content, Footer, Sider } = Layout;
 
 export default class EmployeeManagerInterface extends Component{
@@ -22,14 +23,35 @@ export default class EmployeeManagerInterface extends Component{
                 </Menu>
             </Header>
             <Content style={{ padding: '50px 150px' }}>
-                <Layout style={{ padding: '24px 0', background: '#fff' }}>
-                    <Sider width={200} style={{ background: '#fff' }}>
-                        <MenuList/>
-                    </Sider>
-                    <Content style={{ padding: '0 24px', minHeight: 280 }}>
-                        <Employees/>
-                    </Content>
-                </Layout>
+                <BrowserRouter>
+                    <Layout style={{ padding: '24px 0', background: '#fff' }}>
+                        <Sider width={200} style={{ background: '#fff' }}>
+                            {/*<MenuList/>*/}
+                            <Menu defaultSelectedKeys={['1']} >
+                                    <Menu.Item key="1" >
+                                        <Link to="/Employees">
+                                            <Icon type="form" />员工管理
+                                        </Link>
+                                    </Menu.Item>
+
+
+                                    <Menu.Item key="2" >
+                                        <Link to="/ParkingLots">
+                                            <Icon type="form" />停车场管理
+                                        </Link>
+                                    </Menu.Item>
+                                <Menu.Item key="3" ><Icon type="form" />停车员管理</Menu.Item>
+                                <Menu.Item key="4" ><Icon type="form" />停车场Dashboard</Menu.Item>
+                                <Menu.Item key="5" ><Icon type="form" />订单管理</Menu.Item>
+                            </Menu>
+                        </Sider>
+                        <Content style={{ padding: '0 24px', minHeight: 280 }}>
+                            <Route exact path="/Employees" component={Employees} />
+                            <Route path="/ParkingLots" component={About} />
+                            {/*<Employees/>*/}
+                        </Content>
+                    </Layout>
+                </BrowserRouter>
             </Content>
             <Footer style={{ textAlign: 'center' }}>
                 Parking System ©2018 Created by OOCL.ITA.OverWatcher
@@ -38,3 +60,8 @@ export default class EmployeeManagerInterface extends Component{
         );
     }
 }
+const About = () => (
+    <div>
+        <h2>About</h2>
+    </div>
+);
