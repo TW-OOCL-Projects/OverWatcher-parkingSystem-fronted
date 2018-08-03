b8f479302327661dbf53e19ce5bc65e31a9bc620
Eugene-Armstrong <1020454354@qq.com>
2018-08-03 14:34:53 +0800

    [Jeffery] fix select bug with search input

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -28,31 +28,34 @@ const columns = [{
         if (record.command === "无人处理") {
             return (
                 <span>
-        <a className="ant-dropdown-link">指派</a>
+        <a className="ant-dropdown-link" onClick={this.assignOrder}>指派</a>
         </span>
             )
         }
     },
 }];
 
+const assignOrder =(e) =>{
+    alert(1);
+}
 
 export default class Orders extends Component {
+    state = {selected:"type"};
     render() {
         const datas = (this.props.orders).map((order, index) => {
             const {id, carId, type, status} = order;
             return {key: index, id, carId, type, status, command: status}
         });
-        let selected = "type";
         return (
             <div>
                 <Col span={204} style={{textAlign: "right"}}>
-                    <Select defaultValue="类型" style={{width: 120}} onChange={value=>selected=value}>
+                    <Select defaultValue="类型" style={{width: 120}} onChange={value=>{this.setState({selected:value})}}>
                         <Option value="type">类型</Option>
                         <Option value="status">状态</Option>
                         {/*<Option value="id">Id</Option>*/}
                     </Select>&nbsp;&nbsp;
                     <Search prefix={<Icon type="search" style={{color: 'rgba(0,0,0,.25)'}}/>}
-                            onSearch={value => this.selectedByConditions(value,selected)} style={{width: 200}} enterButton="搜索"/>
+                            onSearch={value => this.selectedByConditions(value,this.state.selected)} style={{width: 200}} enterButton="搜索"/>
                 </Col>
                 <Table bordered columns={columns} dataSource={datas} style={{marginTop: "20px"}}/>
             </div>
